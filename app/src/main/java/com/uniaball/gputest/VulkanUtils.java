cbb6ea735964773ef4e6feb24976ccb8e60ecbce
Uniaball <2551733616@qq.com>
2025-08-17 22:46:36 +0800

    [Fix] (VulkanTest): Fix the use of incorrect vulkan detection flag

--- app/src/main/java/com/uniaball/gputest/VulkanUtils.java
+++ app/src/main/java/com/uniaball/gputest/VulkanUtils.java
@@ -5,11 +5,13 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 
 public class VulkanUtils {
-	public static boolean isSupported(Activity activity) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-			return activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_LEVEL)
-			&& activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_VERSION);
-		}
-		return false;
-	}
+    public static boolean isSupported(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // 使用正确的 Vulkan 检测标志
+            return activity.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_VULKAN_HARDWARE_LEVEL
+            );
+        }
+        return false;
+    }
 }
